92a9da0d3f02bdbaa20f00b043d0db6b5326eb83
Septentrion50 <erwan56lecorre@gmail.com>
2021-11-01 17:56:10 +0100

    Feature: Fetch to GitHub REST API

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-const octokit = new Octokit({ auth: `` });
+require('dotenv').config();
+const { Octokit, App, Action } = require("octokit");
+
+const octokit = new Octokit({ auth: `${process.env['GH_ACCESS_TOKEN']}` });
+
+const api_response = async() => {
+				const res = await octokit.request('GET /search/issues?q=is:open');
+	console.log(res.data.items[0]);
+};
+api_response();
+
