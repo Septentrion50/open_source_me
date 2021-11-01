bf365d8f049b5d26e85ec0b9237c213a5112e1ce
Septentrion50 <erwan56lecorre@gmail.com>
2021-11-01 18:51:20 +0100

    Feature: Graphql API search
    Co-authored-by: Jocelin Queau <queau.j@gmail.com>

--- index.js
+++ index.js
@@ -1,10 +1,52 @@
-require('dotenv').config();
+require("dotenv").config();
 const { Octokit, App, Action } = require("octokit");
 
-const octokit = new Octokit({ auth: `${process.env['GH_ACCESS_TOKEN']}` });
+const octokit = new Octokit({ auth: `${process.env["GH_ACCESS_TOKEN"]}` });
 
-const api_response = async() => {
-				const res = await octokit.request('GET /search/issues?q=is:open');
-	console.log(res.data.items[0]);
+let repo = [];
+
+const api_response = async () => {
+	const res = await octokit.request("GET /search/issues?q=is:open");
+	const repoUrl = res.data.items[0].repository_url;
+	repo = repoUrl.split("/");
+	const repoOwner = repo[4];
+	const repoName = repo[5];
+	console.log(repoOwner, repoName);
+	const data =
+		await octokit.graphql(`
+		query lookAtRepos ($owner: String!, $name: String!) { 
+			repository(owner:$owner, name:$name){
+				url
+				name
+				description
+				descriptionHTML
+    			shortDescriptionHTML
+				owner{
+					id
+					login
+					avatarUrl
+				}
+				languages(first:3){
+					totalSize
+				edges{
+					size
+				  node{
+						 name
+						 color
+					  }
+					}
+				nodes{
+				  name
+				  color
+				}
+			  }
+			}
+		  }`, {
+			owner: `${repoOwner}`,
+			name: `${repoName}`,
+		});
+	console.log(data);
+	console.log(data.repository.languages.edges);
+	console.log(data.repository.languages.nodes);
 };
 api_response();
