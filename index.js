a0fa7b645eabb09873a08df0c3a6b11ec140bc79
Septentrion50 <erwan56lecorre@gmail.com>
2021-11-01 17:25:03 +0100

    Feature: API fetch, not functional

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-const fetch = require('node-fetch');
-const graph = require('graphql');
+import fetch from 'node-fetch';
+import {gql} from 'graphql';
 
 //TODO Authenticate on APIS.
+
+const data = {
+				'method': 'get',
+				'headers': {
+								'Authorization': `${process.env['GH_ACCESS_TOKEN']}`
+				}
+}
+
+const api_response = fetch(`https://api.github.com`, data)
+
+console.log(api_response)
