a7f0a69bc9cabeb4bafeaf84cfe623ce4c62db22
Jocelin Quéau <jocelinqueau@MacBook-Pro-de-Jocelin.local>
2021-11-03 16:16:52 +0100

    adding a query file

---
+++ query.ts
@@ -0,0 +1,40 @@
+require("dotenv").config();
+import { Octokit, App } from "octokit";
+
+
+const octokit = new Octokit({ auth: `${process.env["GH_ACCESS_TOKEN"]}` });
+
+interface RestIssueRObject {
+    url: string;
+    owner: string;
+    repo: string;
+  }
+  
+  const getRESTIssues = async (
+    queryParams: string
+  ): Promise<RestIssueRObject[]> => {
+    const response = await octokit.request(`GET /search/issues?${queryParams}`);
+    const repositoriesUrls: RestIssueRObject[] = [];
+    response.data.items.forEach((repo: any) => {
+      const splittedRepoUrl = repo.repository_url.split("/");
+      repositoriesUrls.push({
+        url: repo.repository_url,
+        owner: splittedRepoUrl[4],
+        repo: splittedRepoUrl[5],
+      });
+    });
+    return repositoriesUrls;
+  };
+
+
+const getRESTRepository = async(owner:string, repo:string):Promise<any> => {
+        const response1 = await octokit.request(`GET /repos/${owner}/${repo}`)
+        const response3 = await octokit.request(`GET /repositories`)
+
+        console.log(response1.data.topics)
+}
+
+(async()=>{
+    const repos = await getRESTIssues("q=is:open")
+    getRESTRepository(repos[0].owner, repos[0].repo)
+})()
