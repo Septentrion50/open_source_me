8449f882dd0222e726d7b16d95e3193dd9112fed
Septentrion50 <erwan56lecorre@gmail.com>
2021-11-15 18:51:25 +0100

    Feature: Parse query

--- src/apiCalls.ts
+++ src/apiCalls.ts
@@ -1,46 +1,49 @@
 import { Octokit } from "octokit";
-import { RestIssueRObject, RepoInfo  } from "./type";
-import {extractLanguagesInfo} from './utils'
+import { RestIssueRObject, RepoInfo } from "./type";
+import { extractLanguagesInfo } from "./utils";
 
 const octokit = new Octokit({ auth: `${process.env["GH_ACCESS_TOKEN"]}` });
 
 export const getRESTIssues = async (
-    queryParams: string
-  ): Promise<RestIssueRObject[]> => {
-    const queryone = encodeURIComponent(`is:open`);
-    const querytwo = encodeURIComponent(`language:typescript`);
-    const response = await octokit.request(`GET /search/issues?q=${queryone}+${querytwo}`);
-    console.log(response)
-    const issueData: RestIssueRObject[] = [];
-    response.data.items.forEach((issue: any) => {
-      const splittedRepoUrl = issue.repository_url.split("/");
-      issueData.push({
-        issue_url: issue.url,
-        issue_number: issue.number,
-        repository_url: issue.repository_url,
-        created_at: issue.created_at,
-        updated_at: issue.updated_at,
-        labels: issue.labels,
-        assignees: issue.assignees,
-        repository_owner: splittedRepoUrl[4],
-        repository_name: splittedRepoUrl[5],
-      });
+  queryParams: string
+): Promise<RestIssueRObject[]> => {
+  console.log("REQUEST: ", queryParams);
+  const listQuery = queryParams.split(" ");
+  for (let i = 0; i < listQuery.length; i++) {
+    listQuery[i] = encodeURIComponent(listQuery[i]);
+  }
+  const parsedQuery = listQuery.length > 0 ? 'is%3Aopen' : listQuery.join("+");
+  const response = await octokit.request(`GET /search/issues?q=${parsedQuery}`);
+  console.log("RESPONSE: ", response);
+  const issueData: RestIssueRObject[] = [];
+  response.data.items.forEach((issue: any) => {
+    const splittedRepoUrl = issue.repository_url.split("/");
+    issueData.push({
+      issue_url: issue.url,
+      issue_number: issue.number,
+      repository_url: issue.repository_url,
+      created_at: issue.created_at,
+      updated_at: issue.updated_at,
+      labels: issue.labels,
+      assignees: issue.assignees,
+      repository_owner: splittedRepoUrl[4],
+      repository_name: splittedRepoUrl[5],
     });
-    return issueData;
-  };
-
+  });
+  return issueData;
+};
 
 export const getRepoDetailsGQL = async (
-    repository_url: string
-  ): Promise<RepoInfo> => {
-    const splittedRepoUrl =  repository_url.split("/")
-    console.log("here",repository_url)
-    const repository_owner = splittedRepoUrl[splittedRepoUrl.length - 2];
-    const repository_name = splittedRepoUrl[splittedRepoUrl.length - 1];
-    console.log("repository_owner", repository_owner)
-    console.log("repository_name", repository_name)
-      let { repository } = await octokit.graphql(
-        `
+  repository_url: string
+): Promise<RepoInfo> => {
+  const splittedRepoUrl = repository_url.split("/");
+  console.log("here", repository_url);
+  const repository_owner = splittedRepoUrl[splittedRepoUrl.length - 2];
+  const repository_name = splittedRepoUrl[splittedRepoUrl.length - 1];
+  console.log("repository_owner", repository_owner);
+  console.log("repository_name", repository_name);
+  let { repository } = await octokit.graphql(
+    `
       query lookAtRepos ($owner: String!, $name: String!) {
         repository(owner:$owner, name:$name){
           url
@@ -57,20 +60,19 @@ export const getRepoDetailsGQL = async (
         }
       }
       `,
-        {
-          owner: repository_owner,
-          name: repository_name,
-        }
-      );
-  
-      let repInfo = {
-        totalSize: repository.languages?.totalSize,
-        languages: extractLanguagesInfo(
-          repository.languages.edges,
-          repository.languages?.totalSize
-        ),
-      };
-      console.log("repInfo", repInfo)
-      return repInfo;
+    {
+      owner: repository_owner,
+      name: repository_name,
     }
-    
+  );
+
+  let repInfo = {
+    totalSize: repository.languages?.totalSize,
+    languages: extractLanguagesInfo(
+      repository.languages.edges,
+      repository.languages?.totalSize
+    ),
+  };
+  console.log("repInfo", repInfo);
+  return repInfo;
+};
